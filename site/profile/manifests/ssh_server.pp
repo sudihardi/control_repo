bc4f9e71bc2a5dd747d7c5a84e34bcc22b401332
sudihardi <72848540+sudihardi@users.noreply.github.com>
2021-01-04 11:19:29 +0700

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -12,3 +12,4 @@ class profile::ssh_server {
                 type   => 'ssh-rsa',
                 key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQC4hWRH3kfyKeT37jx5oxE6dBAey37OuMKMYG2k45X7yzsusKhE3Yiyvco987vlHZpfObKujNHv3HUJv2VzCVUwZgXgOdkCHpHRm2VtXYBrsZO2Nslyet5/OlED7rAParhIhfwyhNtTWNxEPISOf8gXpTXO0z4g5HczOGB2LLXqbH48A7i0sgbIo4YTOM4fcCtWJ9QFhq1lDQxX0B5kteSM2lt3paPYikuXoc2wc5j11It0y3EZtiOMT6mtKuA/LKBrjN9gGXGwmnuvZaDMuRmDtC8hwwenf4pSU/ki9td1I4UaAKiBFF7PlftWhlvMxBmkjfOhaZPHZGSuuglJ59cB ',
         }
+      }
